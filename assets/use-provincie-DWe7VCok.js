e6cb701d5ca43b2df7bf13a4fe15beb2f0835041
LisandroEnrici <50123372+LisandroEnrici@users.noreply.github.com>
2026-07-01 14:50:35 +0000

    Deploying to gh-pages from @ Bazeapp/bazeoffice@0db9dadea9e64cc935b529462437996dfa9a4952 🚀

---
+++ assets/use-provincie-DWe7VCok.js
@@ -0,0 +1,1 @@
+import{r as i}from"./framework-BdAjOp9R.js";import{u as o}from"./use-realtime-board-sync-C8QVgoWN.js";import{a8 as a}from"./anagrafiche-api-D7JZc93d.js";const s=["provincie"];function t(){return o({queryKey:s,queryFn:a,staleTime:300*1e3,gcTime:1800*1e3})}function m(){const{data:e}=t();return i.useMemo(()=>(e??[]).map(r=>({value:r.sigla,label:r.sigla})),[e])}export{m as a,t as u};
