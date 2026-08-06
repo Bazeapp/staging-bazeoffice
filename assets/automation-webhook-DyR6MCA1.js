8d6ce9981bdf437d94158ce90f88d5dfbf3b46da
LisandroEnrici <50123372+LisandroEnrici@users.noreply.github.com>
2026-08-06 09:47:54 +0000

    Deploying to gh-pages from @ Bazeapp/bazeoffice@6b92824a91e2a9d52d97cc368d4df853c6aab20e 🚀

---
+++ assets/automation-webhook-DyR6MCA1.js
@@ -0,0 +1,1 @@
+import{w as t,P as r}from"./main-app-PFbrlqqK.js";async function i(o,n,a){const e=await t("run-automation-webhook",{automationId:o,recordId:n,context:a});return r(),e}export{i as r};
