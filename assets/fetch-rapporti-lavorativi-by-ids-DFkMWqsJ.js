ba744f5bb3ab0b04f205cc7d02035c1545956808
LisandroEnrici <50123372+LisandroEnrici@users.noreply.github.com>
2026-08-27 08:38:08 +0000

    Deploying to gh-pages from @ Bazeapp/bazeoffice@d456b1ad618b8057cd9fc93c2ed9f505a96cbd8e 🚀

---
+++ assets/fetch-rapporti-lavorativi-by-ids-DFkMWqsJ.js
@@ -0,0 +1,1 @@
+import{E as t}from"./main-app-C8YPBJU5.js";import{r as i}from"./rpc-rows-BjAMDyfN.js";async function p(r){return r.length===0?t:i("rapporti_lavorativi_by_ids",{p_ids:r})}export{p as f};
